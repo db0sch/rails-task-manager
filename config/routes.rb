fcc77e3a73153235774561158c59123cc6b42d3e
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-10 15:31:00 +0200

    ex1 - completed

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,17 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  # get    "tasks",          to: "tasks#index", as: "tasks"
+
+  # get    "tasks/new",      to: "tasks#new", as: "new_task"
+  # get    "tasks/:id",      to: "tasks#show", as: "tasks_id"
+  # post   "tasks",          to: "tasks#create"
+
+  # get    "tasks/:id/edit", to: "tasks#edit", as: "tasks_edit"
+  # patch  "tasks/:id",      to: "tasks#update"
+
+  # delete "tasks/:id",      to: "tasks#destroy", as: "tasks_delete"
+  resources :tasks, only: [:show, :index, :edit, :update, :new, :create, :destroy]
+ # remplacer par 'ressources'
+
 end
